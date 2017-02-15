27f5a51e2bf089a2934afe676edd465acf3c9e7b
hugofalkman <zilla@hs-macbook-pro.local>
2017-02-15 12:43:22 +0100

    Initial Commit

---
+++ CalculatorStanford/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalculatorStanford
+//
+//  Created by H Hugo Falkman on 2017-02-15.
+//  Copyright © 2017 H Hugo Falkman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
